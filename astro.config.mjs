92b5398a90a308eb04fa6d8682836d41b3d6e6ad
icecloud12 <johnsonjaurigue2@gmail.com>
2023-05-04 10:51:19 +0800

    build: added svelte to the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-import {astroImageTools} from 'astro-imagetools'
-
+import { astroImageTools } from 'astro-imagetools';
 import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroI18next(), astroImageTools]
+  integrations: [tailwind(), astroI18next(), astroImageTools, svelte()]
 });
