88a31472234ca9ecaee83f6632eb0f246d846616
icecloud12 <johnsonjaurigue2@gmail.com>
2023-05-12 10:58:48 +0800

    feat: default SEO attribute values

---
+++ src/customTypes/seo/seoType.ts
@@ -0,0 +1,10 @@
+export type SEOType = {
+  pageTitle?: string;
+  pageDescription?: string;
+  pageKeywords?: string;
+  pageAuthor?: string;
+  ogTitle?: string;
+  ogType?: string;
+  ogImage?: string;
+  ogDescription?: string;
+}
