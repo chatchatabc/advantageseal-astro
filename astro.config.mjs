d5b32a868a59976df1bc793472e0db6c2c4e9bc8
icecloud12 <johnsonjaurigue2@gmail.com>
2023-05-15 18:07:24 +0800

    build: sitemap additional dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import { astroImageTools } from 'astro-imagetools';
 import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
 import svelte from "@astrojs/svelte";
+import sitemap from '@astrojs/sitemap';
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroI18next(), astroImageTools, svelte()]
+  trailingSlash:"always",
+  integrations: [tailwind(), astroI18next(), astroImageTools, svelte(), sitemap()]
 });
