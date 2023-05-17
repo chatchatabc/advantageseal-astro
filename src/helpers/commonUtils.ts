59a53585908f9082e39ca91a3b0fa0db861c2598
icecloud12 <johnsonjaurigue2@gmail.com>
2023-05-17 17:59:14 +0800

    fix: footer links with correct link by language

--- src/helpers/commonUtils.ts
+++ src/helpers/commonUtils.ts
@@ -1,9 +1,13 @@
- export const utilGetCurrentLangauge = (url: string) => {
+export const utilGetCurrentLangauge = (url: string) => {
   // Determine current lang
   const languages = ["en"];
+  let defaultLanguage = "cn";
   let currentLanguage = "cn";
+  var text="";
   languages.forEach((language) => {
     if (url.includes(`/${language}/`)) currentLanguage = language;
   });
-  return "/"+currentLanguage;
+  (defaultLanguage === currentLanguage)? text =  "" : text =  "/"+currentLanguage;
+  // return defaultLanguage:
+  return text;
 };
