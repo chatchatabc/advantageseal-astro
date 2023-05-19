321c87f7f86c356999f1c2feb488cef231bd080b
icecloud12 <johnsonjaurigue2@gmail.com>
2023-05-19 11:34:00 +0800

    chore: added namespace for translation

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -2,6 +2,6 @@
 export default {
   defaultLocale: "cn",
   locales: ["cn", "en"],
-  namespaces: ["navigation","footer","home"],
+  namespaces: ["site-info","navigation","footer","home"],
   defaultNamespace: "navigation",
 };
