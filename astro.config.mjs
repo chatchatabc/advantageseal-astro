b559f253e5c2099bac0aba1f8a216654ce216b6c
icecloud12 <johnsonjaurigue2@gmail.com>
2023-05-26 13:42:01 +0800

    build: mdx build dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,10 @@ import astroI18next from "astro-i18next";
 import svelte from "@astrojs/svelte";
 import sitemap from '@astrojs/sitemap';
 
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  trailingSlash:"always",
-  integrations: [tailwind(), astroI18next(), astroImageTools, svelte(), sitemap()]
+  trailingSlash: "always",
+  integrations: [tailwind(), astroI18next(), astroImageTools, svelte(), sitemap(), mdx()]
 });
