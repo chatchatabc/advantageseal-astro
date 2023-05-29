8124277de0fdb8f2b0ae875130401be0351540c2
icecloud12 <johnsonjaurigue2@gmail.com>
2023-05-29 16:45:10 +0800

    feature: added currentPage function a utility function thats gonna check link item is the current page

--- src/helpers/commonUtils.ts
+++ src/helpers/commonUtils.ts
@@ -1,5 +1,10 @@
 const defaultLanguage = "cn";
 import i18next from "i18next";
+/**
+ * @function - gets the current language from the url
+ * @param {string}url - string variable to be checked
+ * @returns - the current language, blank if currently using the default language
+ */
 export const utilGetCurrentLanguage = (url: string) => {
   const languages = i18next.languages;
   for(var i = 0 ; i < languages.length ; i++){
@@ -10,3 +15,12 @@ export const utilGetCurrentLanguage = (url: string) => {
   return "";
 
 };
+/**
+ * @function - checks if url contains slug
+ * @param {string} url - url to check if it contains slug
+ * @param {string} slug - slug to be checked
+ * @returns {boolean} - true if url contains slug, false otherwise
+ */
+export const utilCurrentPage = (url:string, slug:string) => {
+  return url.includes(slug);
+}
