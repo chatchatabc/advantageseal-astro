33dd31599aeb845803b3b5dcaa7b76321f95d01c
icecloud12 <johnsonjaurigue2@gmail.com>
2023-06-05 17:53:01 +0800

    feat: created newsCategory collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,7 +5,7 @@ import i18next from 'astro-i18next';
 const newsCollection = defineCollection({
   schema: z.object({
     title : z.string(),
-    category : z.string(),
+    newsCategory : z.string(),
     pubDate : z.string(),
     lang: z.string(),
     image: z.object({
@@ -31,8 +31,28 @@ const newsCollection = defineCollection({
   })
 
 });
+const newsCategoryCollection = defineCollection({
+  schema: z.object({
+    name:z.string(),
+    title: z.string(),
+    lang:z.string(),
+    pageTitle: z.string().nullable(),
+    pageDescription: z.string().nullable(),
+    pageKeywords: z.string().nullable(),
+    pageAuthor: z.string().nullable(),
+    ogTitle: z.string().nullable(),
+    ogType: z.string().nullable(),
+    ogDescription: z.string().nullable(),
+    ogImage: z.string().nullable(),
+    twitterCardType: z.string().nullable().nullable(),
+    twitterTitle: z.string().nullable(),
+    twitterDescription: z.string().nullable(),
+    twitterImage: z.string().nullable(),
+  }),
+});
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
   'news': newsCollection,
+  'news-categories': newsCategoryCollection
 };
