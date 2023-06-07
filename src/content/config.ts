b128e08d296faa085da10e0e5f56d78ba5b27489
icecloud12 <johnsonjaurigue2@gmail.com>
2023-06-07 17:39:54 +0800

    chore: added productCollection might be useless but making it in advance

--- src/content/config.ts
+++ src/content/config.ts
@@ -79,10 +79,33 @@ const solutionsCollection = defineCollection({
   })
 
 });
+const productCollection = defineCollection({
+  schema: z.object({
+    title : z.string(),
+    lang: z.string(),
+    pageTitle: z.string().nullable(),
+    pageDescription: z.string().nullable(),
+    pageKeywords: z.string().nullable(),
+    pageAuthor: z.string().nullable(),
+    ogTitle: z.string().nullable(),
+    ogType: z.string().nullable(),
+    ogDescription: z.string().nullable(),
+    ogImage: z.string().nullable(),
+    twitterCardType: z.string().nullable().nullable(),
+    twitterTitle: z.string().nullable(),
+    twitterDescription: z.string().nullable(),
+    twitterImage: z.string().nullable(),
+    bannerImage: z.string().nullable(),
+    productSlug: z.string().nullable(),
+    productCategory: z.string()
+  })
+
+});
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
   'news': newsCollection,
   'news-categories': newsCategoryCollection,
   'solutions': solutionsCollection,
+  'products': productCollection
 };
