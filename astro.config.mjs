e4a2f8874de84ae3187ba86e170be8dcba2b49a7
icecloud12 <johnsonjaurigue2@gmail.com>
2023-06-15 11:31:40 +0800

    build: react integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,19 +4,13 @@ import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
 import svelte from "@astrojs/svelte";
 import sitemap from "@astrojs/sitemap";
-
 import mdx from "@astrojs/mdx";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   trailingSlash: "always",
   site: "https://advantageseals-astro.pages.dev",
-  integrations: [
-    tailwind(),
-    astroI18next(),
-    astroImageTools,
-    svelte(),
-    sitemap(),
-    mdx(),
-  ],
+  integrations: [tailwind(), astroI18next(), astroImageTools, svelte(), sitemap(), mdx(), react()]
 });
