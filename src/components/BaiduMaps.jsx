7e5518cca973023d15b2c4a533e06747d65eb677
icecloud12 <johnsonjaurigue2@gmail.com>
2023-06-15 13:04:18 +0800

    chore: temp commit

---
+++ src/components/BaiduMaps.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Map, APILoader } from "@uiw/react-baidu-map";
+
+ const BaiduMap = () => (
+  
+
+
+  
+
+    <div style={{ width: '100%' }}>
+      <APILoader akay="eYpCTECSntZmw0WyoQ7zFpCRR9cpgHFG">
+        <Map autoLocalCity style={{ height: 350 }} />
+      </APILoader>
+    </div>
+
+
+
+);
+export default BaiduMap;
