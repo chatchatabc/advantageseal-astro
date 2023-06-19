456e0e982b0e10bde27ad6f80811f7e5aafcf2c7
icecloud12 <johnsonjaurigue2@gmail.com>
2023-06-19 15:13:23 +0800

    feat: baidu map react component

--- src/components/BaiduMaps.jsx
+++ src/components/BaiduMaps.jsx
@@ -1,18 +1,31 @@
-import React from "react";
-import { Map, APILoader, NavigationControl } from "@uiw/react-baidu-map";
+import React, {useState} from "react";
 
- const BaiduMap = () => (
+import { Label, useLabel } from '@uiw/react-baidu-map';
+import { Map, APILoader, NavigationControl, Marker, Provider, InfoWindow } from "@uiw/react-baidu-map";
+
+const CustomMarker = ({content,header})=>{
+  const [labelvisible, setVisible] = useState(true);
+  const iconShapePoint = new BMap.Symbol(BMap_Symbol_SHAPE_POINT, {
+  scale: 1, // 图标缩放大小
+  fillColor: "red", // 填充颜色
+  fillOpacity: 1, // 填充透明度
+  });
+  console.log(header, content)
+  return <Map center={{lng:121.618597,lat:29.815308}} zoom={18} style={{ height: 350 }} widget={['NavigationControl']}>
+      <Marker visiable={true} position={{lng:121.618597,lat:29.815308}} icon={iconShapePoint} onClick={()=>{setVisible(!labelvisible)}} />
+      <Label visiable={labelvisible} position={{lng:121.618597,lat:29.815308}} content={`<div><h3 style='color:red; font-weight:bold'>${header}</h3><p>${content}</p></div>`} style={{padding: "4px"}}/>
+    </Map>
   
+};
 
+const BaiduMap = (props) => (
+  <div style={{ width: '100%' }}>
+    <APILoader akay="eYpCTECSntZmw0WyoQ7zFpCRR9cpgHFG">
+      <Provider>
+        <CustomMarker content={props.content} header={props.header}/>
 
-  
-
-    <div style={{ width: '100%' }}>
-      <APILoader akay="eYpCTECSntZmw0WyoQ7zFpCRR9cpgHFG">
-        <Map center={{lng:121.618918,lat:29.815475}} zoom={18} style={{ height: 350 }} widget={['NavigationControl']}/>
-      </APILoader>
-    </div>
-
-
+      </Provider>
+    </APILoader>
+  </div>
 );
 export default BaiduMap;
