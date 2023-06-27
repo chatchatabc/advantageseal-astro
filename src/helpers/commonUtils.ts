4a89ec6c7bac5a05ff1983ad92596d21d3263665
BoJoNVi <bonjomontes@gmail.com>
2023-06-27 10:01:24 +0800

    feat: implemented utils for creating breadcrumbs

--- src/helpers/commonUtils.ts
+++ src/helpers/commonUtils.ts
@@ -7,13 +7,12 @@ import i18next from "i18next";
  */
 export const utilGetCurrentLanguage = (url: string) => {
   const languages = i18next.languages;
-  for(var i = 0 ; i < languages.length ; i++){
-    if(url.includes(`/${languages[i]}/`)){
-      return "/"+languages[i];
+  for (var i = 0; i < languages.length; i++) {
+    if (url.includes(`/${languages[i]}/`)) {
+      return "/" + languages[i];
     }
   }
   return "";
-
 };
 /**
  * @function - checks if url contains slug
@@ -21,26 +20,72 @@ export const utilGetCurrentLanguage = (url: string) => {
  * @param {string} slug - slug to be checked
  * @returns {boolean} - true if url contains slug, false otherwise
  */
-export const utilCurrentPage = (url:string, slug:string) => {
+export const utilCurrentPage = (url: string, slug: string) => {
   return url.includes(slug);
-}
+};
 /**
- * 
+ *
  * @param url - url to be trimmed
  * @returns - url defaulted to default language
  */
-export const trimLanguageFromLink = (url:string) => {
+export const trimLanguageFromLink = (url: string) => {
   const languages = i18next.languages;
   //language is found in the start of the url
   let links = url.split("/");
   links.pop();
   links.shift();
-  
 
-  if( links.length && languages.includes(links[0]) ){
+  if (links.length && languages.includes(links[0])) {
     links.shift();
-    
   }
   let joinedLink = links.join("/");
   return joinedLink;
+};
+
+export function utilChangeLanguageUrl(url: string, language: string) {
+  const languages = i18next.languages;
+  let links = url.split("/");
+  links.pop();
+  links.shift();
+  if (links.length && languages.includes(links[0])) {
+    links.shift();
+  }
+  links.unshift(language);
+  let joinedLink = links.join("/");
+  return joinedLink;
+}
+
+export function utilNormalizeUrl(url: string) {
+  if (!url.endsWith("/")) {
+    url += "/";
+  }
+  return url;
+}
+
+export function utilBreadcrumbsFromUrl(url: string) {
+  const language = i18next.language;
+  let links = url.split("/");
+  links.pop();
+  if (links.includes(language)) {
+    links.splice(1, 1);
+  }
+
+  let combinedLink = language !== defaultLanguage ? "/" + language : "";
+  let breadcrumbs = links.map((link) => {
+    if (link === "") {
+      return {
+        name: "Home",
+        href: combinedLink === "" ? "/" : combinedLink,
+      };
+    }
+    combinedLink += "/" + link;
+    return {
+      name: link,
+      href: combinedLink,
+    };
+  });
+
+  console.log(breadcrumbs);
+
+  return breadcrumbs;
 }
