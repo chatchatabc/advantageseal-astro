877eb8fa3c11acd2dc5bb5e448df60d1321a94fb
BoJoNVi <bonjomontes@gmail.com>
2023-06-29 14:05:50 +0800

    improv: added responsive design for the innersubnav
    Fixes #5

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,18 @@ module.exports = {
     extend: {
       colors: {
         "text-primary": "#434343",
-        "red-primary":"#cc0000"
+        "red-primary": "#cc0000",
+        p50: "#f3f7fc",
+        p100: "#e6eff8",
+        p200: "#c8ddef",
+        p300: "#98c0e1",
+        p400: "#61a0cf",
+        p500: "#3d84ba",
+        p600: "#2a6495",
+        p700: "#25547f",
+        p800: "#22496a",
+        p900: "#213d59",
+        p950: "#16283b",
       },
     },
   },
