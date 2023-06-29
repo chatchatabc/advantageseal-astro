c2b160efa165919d995aa32d703fe97a586c692c
BoJoNVi <bonjomontes@gmail.com>
2023-06-29 10:16:45 +0800

    build: reconfigure the astro imagetools to use webp format

--- astro-imagetools.config.mjs
+++ astro-imagetools.config.mjs
@@ -3,4 +3,7 @@ import { defineConfig } from "astro-imagetools/config";
 export default defineConfig({
   loading: "eager",
   layout: "fill",
+  format: "webp",
+  fallbackFormat: "webp",
+  includeSourceFormat: false,
 });
